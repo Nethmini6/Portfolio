c384b33e2ae1d5d26b7849224610d51b6d8d1838
Nethmini6 <164535968+Nethmini6@users.noreply.github.com>
2025-10-16 01:05:58 +0530

    Initial commit - portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    string: ["Frontend Developer" , "YouTuber" , "web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
